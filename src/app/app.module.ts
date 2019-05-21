666ee08f560e2c939e4783bf9121989119e67dd3
serhii.azizov <serhii.azizov@continuum.net>
2019-05-21 12:20:49 +0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { D5Component } from './d5/d5.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    D5Component
   ],
   imports: [
     BrowserModule,
